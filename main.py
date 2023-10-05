e23d7aecd53f99b5986f397a8a7ca8b9d942483e
jacek.lange <jacek.lange@gmail.com>
2023-10-05 08:44:51 +0200

    split to findall

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import collections
 from abc import abstractmethod, ABCMeta
 from collections import defaultdict
 from typing import Iterable, Tuple, Generator, Optional
@@ -12,52 +13,12 @@ import unicodedata
 StrGenerator = Generator[str, None, None]
 
 SPLIT_WORDS_PATTERN = re.compile(r'(\W|\s)+', re.MULTILINE)
+FIND_WORDS_PATTERN = re.compile(r'\w+', re.MULTILINE)
 
 
-class IWordCollectionVisitor(metaclass=ABCMeta):
-    def __init__(self, next_visitor: Optional["IWordCollectionVisitor"] = None):
-        self.next_visitor = next_visitor
 
-    def visit_words(self, words: StrGenerator) -> StrGenerator:
-        for word in words:
-            if self.next_visitor:
-                for next_word in self.next_visitor(word):
-                    for w in self.visit(next_word):
-                        yield w
-            else:
-                for w in self.visit(word):
-                    yield w
-
-    @abstractmethod
-    def visit(self, word: str) -> StrGenerator:
-        ...
-
-
-class WordNormalizer(IWordCollectionVisitor):
-    def visit(self, word: str) -> StrGenerator:
-        yield word.lower()
-
-
-class WordCounter(IWordCollectionVisitor):
-
-    def __init__(self, next_visitor: Optional[IWordCollectionVisitor] = None):
-        super().__init__(next_visitor)
-        self.word_counts = {}
-
-    def visit(self, word: str) -> StrGenerator:
-        if word not in self.word_counts:
-            self.word_counts[word] = 0
-        self.word_counts[word] += 1
-        yield word
-
-
-class Splitter(IWordCollectionVisitor):
-
-    def visit(self, text: str) -> StrGenerator:
-        for word_candidate in SPLIT_WORDS_PATTERN.split(text):
-            if word_candidate == "" or word_candidate == " ":
-                continue
-            yield word_candidate
+def normalize_word(word: str) -> str:
+    return word.lower()
 
 
 class MyHTMLParser(HTMLParser):
@@ -66,9 +27,10 @@ class MyHTMLParser(HTMLParser):
     finished = False
     letter_pattern = re.compile(r'\w', re.MULTILINE)
 
-    def __init__(self, visitor: IWordCollectionVisitor, **kwargs):
+    def __init__(self, counter: collections.Counter, **kwargs):
         super().__init__(**kwargs)
-        self._visitor = visitor
+        self._counter = counter
+
 
     def handle_starttag(self, tag: str, attrs: Iterable[Tuple[str, str]]):
         if tag == 'body':
@@ -82,24 +44,30 @@ class MyHTMLParser(HTMLParser):
     def handle_data(self, data: str):
         if self.started == False or self.finished == True:
             return
+        if data == "":
+            return
         text_part = unicodedata.normalize("NFC", data)  # https://docs.python.org/3/library/unicodedata.html#unicodedata.normalize
         if self.letter_pattern.search(text_part) is None:
             return
-        if self.closest_parent_tag not in {"style", "script", "head", "meta", "title", "html"}:
+        if self.closest_parent_tag not in {"style", "link", "script", "head", "meta", "title", "html"}:
             print("Encountered some text :", text_part)
-            self._visitor.visit_words([text_part])
-
+            self._counter.update(
+                map(
+                    normalize_word,
+                    FIND_WORDS_PATTERN.findall(text_part))
+            )
 
 def count_human_readable_words_in_webpage(url):
-    word_counter = WordCounter(WordNormalizer(Splitter()))
-    parser = MyHTMLParser(visitor=word_counter)
+    word_counter = collections.Counter()
+
+    parser = MyHTMLParser(word_counter)
     with httpx.stream("GET", url) as r:
         for text in r.iter_text():
             parser.feed(text)
             if parser.finished:
                 break
 
-    print(f"{repr(word_counter.word_counts)}")
+    print(f"{repr(word_counter.most_common(1000))}")
     parser.close()
 
 
